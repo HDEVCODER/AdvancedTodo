6628cd67d19454d4786644f796db0f6e9a924696
alhamodi65 <91828081+alhamodi65@users.noreply.github.com>
2022-05-11 02:25:18 +0200

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,27 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:hive_flutter/hive_flutter.dart';
-import 'package:hivetodo/cubit/cubit.dart';
-import 'package:hivetodo/model/todo_model.dart';
-import 'package:hivetodo/screens/screens.dart';
-import 'package:path_provider/path_provider.dart';
+    import 'package:flutter/material.dart';
+    import 'package:flutter_bloc/flutter_bloc.dart';
+    import 'package:hive_flutter/hive_flutter.dart';
+    import 'package:hivetodo/cubit/cubit.dart';
+    import 'package:hivetodo/model/todo_model.dart';
+    import 'package:hivetodo/screens/screens.dart';
+    import 'package:path_provider/path_provider.dart';
 
-const String todoBoxName = "todos";
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  final document = await getApplicationDocumentsDirectory();
-  Hive.init(document.path);
-  Hive.registerAdapter(TodoModelAdapter());
-  await Hive.initFlutter();
-  await Hive.openBox<TodoModel>(todoBoxName);
-  runApp(const MyApp());
-}
+    const String todoBoxName = "todos";
+    Future<void> main() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    final document = await getApplicationDocumentsDirectory();
+    Hive.init(document.path);
+    Hive.registerAdapter(TodoModelAdapter());
+    await Hive.initFlutter();
+    await Hive.openBox<TodoModel>(todoBoxName);
+    runApp(const MyApp());
+    }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+    class MyApp extends StatelessWidget {
+    const MyApp({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
+    @override
+    Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
         BlocProvider(create: (context) => TodoCubit()..getBox()),
@@ -34,5 +34,5 @@ class MyApp extends StatelessWidget {
         home: HomeScreen(),
       ),
     );
-  }
-}
+    }
+    }
